b89319db6709e3884cddc8753f88e1e702d1ef06
bocco97 <48329618+bocco97@users.noreply.github.com>
2019-10-18 17:10:35 +0200

    Case classes addedd

--- src/main/scala/it/unipi/meteorites/GeoElaborator.scala
+++ src/main/scala/it/unipi/meteorites/GeoElaborator.scala
@@ -15,9 +15,20 @@ import sun.net.www.protocol.https.DefaultHostnameVerifier
 import scala.util.parsing.json._
 //spark-submit --master yarn --deploy-mode client --class "GeoElaborator" GeoElaborator-1.0-SNAPSHOT.jar
 //spark-submit --master yarn --deploy-mode cluster --class it.unipi.meteorites.GeoElaborator GeoElaborator-1.0-SNAPSHOT.jar
+//{"osm_type":"way","osm_id":667034130,"lat":"46.7634391","lon":"-70.1199366","display_name":"14, Sainte-Lucie-de-Beauregard, Montmagny (MRC), Chaudière-Appalaches, Québec, Canada","address":{"path":"14","village":"Sainte-Lucie-de-Beauregard","county":"Montmagny (MRC)","region":"Chaudière-Appalaches","state":"Québec","country":"Canada","country_code":"ca"},"boundingbox":["46.7570163","46.7723947","-70.1254374","-70.0932999"]}
 object GeoElaborator {
+
   val log = LoggerFactory.getLogger(GeoElaborator.getClass.getName)
-  case class Event(lat: String, lon:String,id:String)
+
+  case class AdditionalInfo(Name: String, State:String, Country:String)
+
+  case class Meteorite(year:String, mass: String, lat:String, lon:String, recclass:String, resolved:Boolean, info:AdditionalInfo)
+
+  def string2map(s:String):Map[String,String]={
+    val obj = JSON.parseFull(s)
+    val map = obj.get.asInstanceOf[Map[String, String]]
+    map
+  }
 
   def restCall(lat:String,lon:String,url_0:String,url_1:String):String={
     val filekeys = this.getClass.getResourceAsStream("/example.jks")
@@ -42,7 +53,7 @@ object GeoElaborator {
       val buffreader = new BufferedReader(new InputStreamReader(urlconn.getInputStream))
       var line = buffreader.readLine()
       while (line != null) {
-        res = res+line
+        res = res + line
         line = buffreader.readLine()
       }
       buffreader.close()
@@ -53,6 +64,51 @@ object GeoElaborator {
     res
   }
 
+  def elaborate(result:String, event:String):Meteorite={
+    val response = string2map(result)
+    val evt = string2map(event)
+    if(response.get("error").isDefined){
+      val year = evt.get("year").get
+      val mass = String.valueOf(evt.get("mass").get)
+      val reclat = evt.get("reclat").get
+      val reclon = evt.get("reclong").get
+      val reccl = evt.get("recclass").get
+      val m = Meteorite(year,mass,reclat,reclon,reccl,false,null)
+      println("\nMeteorite fell in the water")
+      println(m)
+      println("\n")
+      m
+    }
+    else{
+      val info = response.get("address").get.asInstanceOf[Map[String,String]]
+      var state = ""
+      var country = ""
+      try {
+        state = info.get("state").get
+      }
+      catch{
+        case _ : Throwable => ()
+      }
+      try {
+       country = info.get("country").get
+      }
+      catch{
+        case _ : Throwable => ()
+      }
+      val name = response.get("display_name").get
+      val additionalInfo = AdditionalInfo(name,state,country)
+      val year = evt.get("year").get
+      val mass = String.valueOf(evt.get("mass").get)
+      val reclat = evt.get("reclat").get
+      val reclon = evt.get("reclong").get
+      val reccl = evt.get("recclass").get
+      val m = Meteorite(year,mass,reclat,reclon,reccl,true,additionalInfo)
+      println("\n")
+      println(m)
+      println("\n")
+      m
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     /*************************************** Parametri di configurazione ***********************************/
@@ -65,13 +121,12 @@ object GeoElaborator {
     val url_0 = "https://nominatim.openstreetmap.org/reverse?format=json&"
     val url_1 = "&zoom=18&addressdetails=1"
     val prop = new Properties()
-    val currentDirectory = new java.io.File(".").getCanonicalPath
     prop.load(this.getClass().getResourceAsStream("/config.properties"))
     localRun = prop.getProperty("LOCAL_RUN")
     bootstrap_server = prop.getProperty("BOOTSTRAP_SERVER")
     group_id = prop.getProperty("GROUP_ID")
     auto_offset_reset = prop.getProperty("AUTO_OFFSET_RESET")
-    topic = prop.getProperty("TOPIC")
+    topic = prop.getProperty("READING_TOPIC")
     val conf = if (localRun=="true") new SparkConf().setMaster("local[*]") else new SparkConf()
     val sc = new SparkContext(conf.setAppName("GeoElaborator"))
     val ssc = new StreamingContext(sc, Seconds(1))
@@ -85,12 +140,12 @@ object GeoElaborator {
     val record1 = record.transform(rdd => rdd).map(_._2)
     record1.foreachRDD(x => {
         try {
-          val obj = JSON.parseFull(x.first())
-          val map = obj.get.asInstanceOf[Map[String, String]]
+          val evt = x.first()
+          val map = string2map(evt)
           val lat = map.get("reclat").get
           val lon = map.get("reclong").get
           val result = restCall(lat, lon, url_0, url_1)
-          println("\n"+result+"\n")
+          val meteorite = elaborate(result,evt)
         }
         catch{
           case x:java.lang.UnsupportedOperationException =>{
